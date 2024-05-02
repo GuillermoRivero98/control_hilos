dfc58af4b9c3e138c47646ddd86662413191ba06
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-05-02 18:53:42 -0300

    repo y codigo
    por fin logre crear el repo

---
+++ src/Library/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Library;
+
+using System;
+using System.Threading;
+
+public class Corredor // clase corredor
+{
+    private string nombre;
+    private int distanciaRecorrida;
+    private Random random;
+
+    public Corredor(string nombre) // constructor de la clase corredor
+    {
+        this.nombre = nombre; // se asigna el nombre
+        this.distanciaRecorrida = 0; // se asigna la distancia recorrida
+        this.random = new Random(); // se crea un objeto random
+    }
+
+    public void Correr()// metodo correr
+    {
+        while (this.distanciaRecorrida < 100) // distancia recorrida sea menor a 100
+        {
+            this.distanciaRecorrida += random.Next(1, 10); // se suma un valor aleatorio entre 1 y 10 a la distancia recorrida
+            Console.WriteLine($"{this.nombre} ha recorrido {this.distanciaRecorrida} metros");// se muestra la distancia recorrida
+            Thread.Sleep(1000); // se detiene el hilo por 1 segundo
+        }
+        Console.WriteLine($"{this.nombre} ha llegado a la meta");
+    }
+}
---
+++ src/Program/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Ejercicio Carrera 
+Cree un programa que simule una carrera entre hilos. 
+Cada corredor tendrá un nombre, un atributo que diga qué distancia lleva recorrida y correrá a una 
+velocidad randómicamente definida. 
+Para que se aprecie cómo se desarrolla la carrera cada hilo mostrará su posición actual 
+periódicamente. 
+Una vez que el primer corredor atraviesa la meta todos los corredores deberán dejar de correr y el 
+programa deberá declara el ganador. 
+*/
+
+using System;
+using System.Threading;
+using Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Corredor corredor1 = new Corredor("Corredor 1"); // se crea un objeto corredor
+            Corredor corredor2 = new Corredor("Corredor 2");
+            Corredor corredor3 = new Corredor("Corredor 3");
+            Corredor corredor4 = new Corredor("Corredor 4");
+            Corredor corredor5 = new Corredor("Corredor 5");
+
+            Thread hilo1 = new Thread(corredor1.Correr); // se crea un hilo
+            Thread hilo2 = new Thread(corredor2.Correr);
+            Thread hilo3 = new Thread(corredor3.Correr);
+            Thread hilo4 = new Thread(corredor4.Correr);
+            Thread hilo5 = new Thread(corredor5.Correr);
+
+            hilo1.Start(); // se inicia el hilo
+            hilo2.Start();
+            hilo3.Start();
+            hilo4.Start();
+            hilo5.Start();
+
+            hilo1.Join();
+            hilo2.Join();
+            hilo3.Join();
+            hilo4.Join();
+            hilo5.Join();
+
+            Console.WriteLine("Carrera finalizada"); // se muestra mensaje de finalización
+        }
+    }
+
+
+}
+
+/*
+Se obtiene el mismo resultado si se vuelve a correr el programa? 
+Jus fique.
+
+No se obtiene el mismo resultado, ya que la velocidad de los corredores es aleatoria,
+por lo tanto, el resultado de la carrera siempre sera diferente
+*/
